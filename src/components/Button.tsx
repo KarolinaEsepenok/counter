1f3c2bfc6cad9a8bd16c65bf18de23ef4530f59a
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-07-10 17:39:38 +0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React,{useState} from 'react';
+{/*
+type ButtonIncProps = {
+    name: string
+   callBack:()=>void
+}
+export const ButtonInc = (props: ButtonIncProps) => {
+
+    const onClickHandler=(event:number)=>{
+        props.callBack()
+    }
+
+    return (
+        <div>
+            <button onClick={props.callBack} >{props.name}</button>
+        </div>
+    );
+};
+*/}
